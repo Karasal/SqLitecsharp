5f3115f15946ce97d07b4ac9437dc106929aa1d1
Furkan Aladağ <osman4465@hotmail.com>
2021-03-29 18:52:05 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp3/PeopleForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class PeopleForm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.firstNameText = new System.Windows.Forms.TextBox();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.refreshListbuton = new System.Windows.Forms.Button();
+            this.personlistBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Location = new System.Drawing.Point(81, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Person";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(238, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "List People";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "AD";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "SOYAD";
+            // 
+            // firstNameText
+            // 
+            this.firstNameText.Location = new System.Drawing.Point(84, 142);
+            this.firstNameText.Name = "firstNameText";
+            this.firstNameText.Size = new System.Drawing.Size(100, 20);
+            this.firstNameText.TabIndex = 4;
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(84, 218);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(100, 20);
+            this.lastNameText.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 52);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Add Person";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // refreshListbuton
+            // 
+            this.refreshListbuton.Location = new System.Drawing.Point(84, 333);
+            this.refreshListbuton.Name = "refreshListbuton";
+            this.refreshListbuton.Size = new System.Drawing.Size(100, 52);
+            this.refreshListbuton.TabIndex = 7;
+            this.refreshListbuton.Text = "Refresh List";
+            this.refreshListbuton.UseVisualStyleBackColor = true;
+            this.refreshListbuton.Click += new System.EventHandler(this.refreshListbuton_Click);
+            // 
+            // personlistBox
+            // 
+            this.personlistBox.FormattingEnabled = true;
+            this.personlistBox.Location = new System.Drawing.Point(241, 83);
+            this.personlistBox.Name = "personlistBox";
+            this.personlistBox.Size = new System.Drawing.Size(192, 316);
+            this.personlistBox.TabIndex = 8;
+            // 
+            // PeopleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 454);
+            this.Controls.Add(this.personlistBox);
+            this.Controls.Add(this.refreshListbuton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.firstNameText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "PeopleForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox firstNameText;
+        private System.Windows.Forms.TextBox lastNameText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button refreshListbuton;
+        private System.Windows.Forms.ListBox personlistBox;
+    }
+}
+
---
+++ WindowsFormsApp3/PeopleForm.cs
@@ -0,0 +1,71 @@
+﻿using ClassLibrary2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class PeopleForm : Form
+    {
+        List<PersonModel> People = new List<PersonModel>();
+        public PeopleForm()
+        {
+            InitializeComponent();
+
+            LoadPeopleList();
+        }
+        private void LoadPeopleList()
+        {
+            People = SqliteDataAccess.LoadPeople();
+
+            WireUpPeopleList();
+        }
+
+        private void WireUpPeopleList()
+        {
+            personlistBox.DataSource=null;
+            personlistBox.DataSource= People;
+            personlistBox.DisplayMember = "FullName";
+        }
+
+        private void refreshListbuton_Click(object sender, EventArgs e)
+        {
+            LoadPeopleList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            PersonModel p = new PersonModel();
+
+            p.FirstName = firstNameText.Text;
+            p.LastName = lastNameText.Text;
+
+            SqliteDataAccess.SavePerson(p);
+
+            firstNameText.Text = "";
+            lastNameText.Text = "";
+
+
+
+
+
+            /*
+            string metin = firstNameText.Text;
+            string metin1 = lastNameText.Text;
+            personlistBox1.Items.Add(metin);
+            personlistBox1.Items.Add(metin1);
+            firstNameText.Text = "";
+            lastNameText.Text = "";
+            */
+        }
+
+        
+    }
+}
